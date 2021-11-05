5fbaae207612877135b0b3f6a76142a4773581a7
gyrrm <93772840+gyrrm@users.noreply.github.com>
2021-11-05 18:09:56 +0100

    Update AdelThesisController.java
    Modifying registration operation from GET to POST

--- src/main/java/com/adel/thesis/adelthesis/controller/AdelThesisController.java
+++ src/main/java/com/adel/thesis/adelthesis/controller/AdelThesisController.java
@@ -4,7 +4,7 @@ import com.adel.thesis.adelthesis.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ public class AdelThesisController {
     @Autowired
     private RegistrationService registrationService;
 
-    @GetMapping(value = "/customer/registration", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/customer/registration", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> registration(
                           @RequestHeader(value = "uuid", required = false) final String uuid,
                           @RequestHeader(value = "sourceapp", required = false) final String sourceapp,
